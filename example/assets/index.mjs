a3f4ee96de3b19684ac54143911c055448343854
Jascha <jascha@jaeh.at>
2019-12-06 20:18:02 +0100

    import gdpr for example

--- example/assets/index.mjs
+++ example/assets/index.mjs
@@ -1,12 +1,2 @@
-import path from 'path'
+export * as Gdpr from '../../src/index.mjs'
 
-import * as P from '../../src/index.mjs'
-
-const dirName = new URL(import.meta.url).pathname
-
-const lib = path.resolve(path.join(dirName, '..', '..', '..', 'src', 'lib', 'index.mjs'))
-
-export const Pre = {
-  ...P,
-  lib,
-}
