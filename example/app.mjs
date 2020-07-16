870852255086b702e0e060f9a32280359eb8ee1a
jascha <jascha@jaeh.at>
2020-07-16 23:15:27 +0200

    update logotext

--- example/app.mjs
+++ example/app.mjs
@@ -2,7 +2,8 @@ export const state = {
   title: '@magic-modules/gdpr',
   description:
     'adds a gdpr compliant information popup with minimal intrusion and complexity to your page.',
-  logotext: '@magic-modules/gdpr',
+  logotext: 'Gdpr',
+
   menu: [
     { to: '/#installation', text: 'installation' },
     { to: '/#usage', text: 'usage' },
